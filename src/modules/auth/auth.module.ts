7457fc3dfd32f579d08af95cba99e3ef0979fe0c
Rojalin Barik <rojalinbarik2005@gmail.com>
2026-04-21 21:13:19 +0530

    feat: enhance authentication module with user registration and login functionality

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { User,UserSchema } from './schemas/user.schema';
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule} from "@nestjs/jwt";
 @Module({
+  imports:[
+    MongooseModule.forFeature([
+      {name:User.name,schema:UserSchema},
+    ]),
+    PassportModule,
+    JwtModule.register({
+      secret:process.env.JWT_SECRET,
+      signOptions:{expiresIn:'7d'}
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService]
 })
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,64 @@
-import { Injectable } from '@nestjs/common';
-
+import {
+  BadRequestException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { User } from './schemas/user.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { JwtService } from '@nestjs/jwt';
+import { RegisterDto } from './dto/register.dto.';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login.dto';
+import { access } from 'fs';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    @InjectModel(User.name)
+    private userModel: Model<User>,
+    private jwtService: JwtService,
+  ) {}
+  async register(dto: RegisterDto) {
+    const exists = await this.userModel.findOne({
+      email: dto.email,
+    });
+    if (exists) {
+      throw new BadRequestException('Email already exists');
+    }
+    const hashedPassword = await bcrypt.hash(dto.password, 10);
+    const user = await this.userModel.create({
+      ...dto,
+      password: hashedPassword,
+    });
+    return {
+      message: 'User registered successfully',
+      user,
+    };
+  }
+  async LoginDto(dto: LoginDto) {
+    const user = await this.userModel.findOne({
+      email: dto.email,
+    });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const passwordMatch = await bcrypt.compare(dto.password, user.password);
+    if (!passwordMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    const token = this.jwtService.sign({
+      sub: user._id,
+      email: user.email,
+      role: user.role,
+    });
+    return {
+      message: 'Login Successful',
+      accessToken: token,
+      user: {
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+    };
+  }
+}
